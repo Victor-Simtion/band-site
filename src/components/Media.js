8101db8b0111c77c9a51a7630873a6aabfa7d9b0
Victor-Simtion <simtin.victor@gmail.com>
2023-06-06 14:17:42 +0300

    populated media page

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Poze from '../data/PozeMedia.json'
 
 export function Media() {
   return (
@@ -7,10 +8,16 @@ export function Media() {
         <h1>Media</h1>
       </div>
       <div className='continut'>
-        <div className='continutPrim'>
-        <p>Eu velit qui ad consectetur veniam ipsum tempor dolore incididunt quis irure nisi ex cupidatat. Laborum do consectetur ullamco id eu excepteur duis tempor eiusmod nisi. Adipisicing aliqua minim in reprehenderit veniam proident ad commodo reprehenderit minim sunt.</p>
-        <img alt='image'  src='https://www.thomann.de/blog/wp-content/uploads/2222/05/albumcovers_header_770x425_a2.jpg' />
-        </div>
+            <div className='continutPrim'>
+              <p>Eu velit qui ad consectetur veniam ipsum tempor dolore incididunt quis irure nisi ex cupidatat. Laborum do consectetur ullamco id eu excepteur duis tempor eiusmod nisi. Adipisicing aliqua minim in reprehenderit veniam proident ad commodo reprehenderit minim sunt.</p>
+                  <div className='mediaGrid'>
+                    {Poze && Poze.map(poze=>(
+                      <a href='http://dummyimage.com'>
+                      <img className='mediaPic' key={poze.id} alt={poze.id} src = {`http://dummyimage.com/${poze.poza}`}/> 
+                      </a>
+                    ))}
+                  </div>
+            </div>
       </div>
     </>
   )
